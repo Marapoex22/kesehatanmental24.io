20f8640948e7b361bb254c0e679887c03ee45990
Marapoex22 <126431536+Marapoex22@users.noreply.github.com>
2024-06-15 07:02:23 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title('kesehatan Mental App')
+    st.title('Simple Login App')
 
     # Menampilkan halaman login menggunakan HTML
     with open('login.html', 'r') as file:
@@ -16,20 +16,14 @@ def main():
     if st.button('Login'):
         if username == 'admin' and password == 'password':
             st.success('Login berhasil!')
-            # Navigasi ke halaman diagnosa setelah login berhasil
-            show_diagnosa_page()
+
+            # Setelah login berhasil, arahkan ke halaman diagnosa_form.html
+            with open('diagnosa_form.html', 'r') as file:
+                diagnosa_page = file.read()
+            st.markdown(diagnosa_page, unsafe_allow_html=True)
+
         else:
             st.error('Username atau password salah')
-
-def show_diagnosa_page():
-    st.title('Halaman Diagnosa')
-
-    # Menampilkan halaman diagnosa menggunakan HTML
-    with open('templates/diagnosa.html', 'r') as file:
-        diagnosa_page = file.read()
-    st.markdown(diagnosa_page, unsafe_allow_html=True)
-
-    # Tambahkan logika dan interaksi tambahan di halaman diagnosa di sini
 
 if __name__ == '__main__':
     main()
